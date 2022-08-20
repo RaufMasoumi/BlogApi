f59fb1b9d9eb75586072de21677af42b19a271f0
RaufMasoumi <raouf.rm93@gmail.com>
2022-08-20 14:40:23 +0430

    Added a generic view class to the accounts for avoiding the repetition.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from rest_framework.permissions import IsAdminUser
+from posts.models import Post, Comment, Reply
 from .serializers import CustomUserListSerializer, CustomUserDetailSerializer, UserPostListSerializer, \
     UserCommentListSerializer, UserReplyListSerializer
 from .permissions import IsAdminOrUser, IsUserOrReadOnly, IsUserOrAdminReadOnly
@@ -26,48 +27,55 @@ class UserViewSet(ModelViewSet):
         return permissions
 
 
-class GetUserObjMixin:
-    request = None
-    kwargs = None
+class UserReverseRelationListCreateView(ListCreateAPIView):
+    serializer_class_pattern = 'User_RelatedClass_ListSerializer'
+    reverse_model_class = None
+    reverse_model_user_field_name = 'author'
+    reverse_field_related_name = None
+
+    def get_reverse_field_related_name(self):
+        if self.reverse_field_related_name:
+            return self.reverse_field_related_name
+        lower_model_class_name = self.reverse_model_class.__name__.lower()
+        return lower_model_class_name + 's'
+
+    def get_serializer_class(self):
+        if self.serializer_class:
+            return self.serializer_class
+        model_class_name = self.reverse_model_class.__name__
+        serializer_class_name = self.serializer_class_pattern.replace(
+            '_RelatedClass_', model_class_name
+        )
+        exec(f'serializer_class = {serializer_class_name}')
+        return locals().get('serializer_class')
 
     def get_user_obj(self):
         return get_user(self, or_from_request=True)
 
+    def get_queryset(self):
+        user = self.get_user_obj()
+        queryset = getattr(user, self.get_reverse_field_related_name())
+        return queryset.all()
 
-class UserPostListView(ListCreateAPIView, GetUserObjMixin):
-    serializer_class = UserPostListSerializer
+    def perform_create(self, serializer):
+        kwargs = {self.reverse_model_user_field_name: self.get_user_obj()}
+        serializer.save(**kwargs)
+
+
+class UserPostListView(UserReverseRelationListCreateView):
+    reverse_model_class = Post
     permission_classes = [IsUserOrReadOnly, ]
 
-    def get_queryset(self):
-        user = self.get_user_obj()
-        return user.posts.all()
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.get_user_obj())
-
-
-class UserCommentListView(ListCreateAPIView, GetUserObjMixin):
-    serializer_class = UserCommentListSerializer
+class UserCommentListView(UserReverseRelationListCreateView):
+    reverse_model_class = Comment
     permission_classes = [IsUserOrAdminReadOnly, ]
 
-    def get_queryset(self):
-        user = self.get_user_obj()
-        return user.comments.all()
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.get_user_obj())
-
-
-class UserReplyListView(ListCreateAPIView, GetUserObjMixin):
-    serializer_class = UserReplyListSerializer
+class UserReplyListView(UserReverseRelationListCreateView):
+    reverse_model_class = Reply
+    reverse_field_related_name = 'replies'
     permission_classes = [IsUserOrAdminReadOnly, ]
-
-    def get_queryset(self):
-        user = self.get_user_obj()
-        return user.replies.all()
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.get_user_obj())
 
 
 def get_user(view_obj, or_from_request=False):
