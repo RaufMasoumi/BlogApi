64fae0af9ffe730199f2068ff9318ccefdc3be36
RaufMasoumi <raouf.rm93@gmail.com>
2022-09-15 14:18:56 +0430

    Fixed DATABASE_URL heroku config var error.

--- django_project/settings.py
+++ django_project/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url('DJANGO_DATABASE_URL')
+    'default': env.dj_db_url('DATABASE_URL')
 }
 
 
