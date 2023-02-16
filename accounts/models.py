43d04608f4af9310edffd8ce3253a04458c7c69c
RaufMasoumi <raouf.rm93@gmail.com>
2023-02-16 14:07:00 +0330

    Added slug to the user with default.

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    slug = models.SlugField(max_length=150, default='user-slug')
     phone_number = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
