2250281c2704c7871b8a1bb3e928a313166b187d
RaufMasoumi <raouf.rm93@gmail.com>
2023-05-21 10:20:34 +0330

    Added back4app containers server host domain to the allowed hosts.

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '.b4a.run', '.containers.back4app.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '.b4a.run', '.containers.back4app.com', 'node29a.containers.back4app.com', 'node27a.containers.back4app.com']
 
 
 # Application definition
